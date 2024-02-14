d0c5296332f94ae6ef50dc81366706fb4c1fa54b
CopeOnline <cop21004@byui.edu>
2024-02-13 18:30:03 -0700

    333

--- src/js/locations.mjs
+++ src/js/locations.mjs
@@ -12,9 +12,3 @@ const location_id = dataSource.results.data[0].result_object.location_id
 const location = new ProductDetails(location_id, dataSource);
 location.init();
 
-//js for the product_pages/index.html page
-//get the productid from dynamic searchquery (url) using getParams in utils.js
-//create product Data class from json using ProductData in ProductData.js
-//create a ProductDetails class based on the id and data class.
-//initialize productDetails which builds the product page details
-
