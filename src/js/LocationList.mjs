3628d0817197c1f4e72dd2054fc406fd144f6dff
CopeOnline <cop21004@byui.edu>
2024-02-18 11:54:40 -0700

    add error handling

--- src/js/LocationList.mjs
+++ src/js/LocationList.mjs
@@ -10,20 +10,40 @@ function locationCardTemplate(location){
     </section>`
 }
 
+function locationErrorTemplate(location){
+    return `<section class="error-card">
+    <a href="../index.html">
+        <h3 class="error_name">Whoops, something went wrong</h3>
+        <h3 class="error_results">No Matches Found</h3>
+        <h4 class="error_state">Click Here to Start Over</h4>
+    </a>
+    </section>`
+}
+
 
 export default class LocationList{
     constructor(category){
         this.category = category;
         this.dataSource = getLocalStorage('search');
         this.listElement = document.querySelector(".location-list");
+        this.element = this.dataSource.results.data;
+        this.location = this.element.result_object;
+        this.template = locationCardTemplate;
     }
     async init() {
-        this.element = this.dataSource.results.data
-        this.location = this.element.result_object
-        
-        this.renderList(this.element)
+        if (this.dataSource === null ) {
+            this.element = [{category: 0}]
+            this.template = locationErrorTemplate;
+        };
+        if (Object.keys(this.element).length === 0){
+            this.element = [{category: 0}]
+            this.template = locationErrorTemplate;
+        };
+        this.renderList(this.template, this.element)
+        }
+    renderList(template, element){
+        console.log(element)
+        renderListWithTemplate(template, this.listElement, element, 'afterbegin', false);
     }
-    renderList(element){
-        renderListWithTemplate(locationCardTemplate, this.listElement, element, 'afterbegin', false);
-    }
-}
+};
+
